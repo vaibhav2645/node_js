7915192cf142f863f31e51e92aa969ac878f9477
vaibhav2645 <70876520+vaibhav2645@users.noreply.github.com>
2021-06-05 20:26:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const mainRoute = require("./routes/app");
+const route = require("./routes/app");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(mainRoute);
+app.use(route);
 
 app.listen(3000, () => {
   console.log(`server started at 3000`);
